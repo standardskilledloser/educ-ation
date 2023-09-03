b93ddbb0d06d6402de07b445692088bd1987ccbe
Thesecondalthowcool <125369193+Thesecondalthowcool@users.noreply.github.com>
2023-09-03 19:17:45 -0400

    Add files via upload

---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/astroid/go/',
+    bare: 'https://bare.mathlearning.tech/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
